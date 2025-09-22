841e40be1c8a1292a5354167d70c1a518c196692
Oleg <olegonyshk00@gmail.com>
2025-09-22 20:52:31 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+exchange_rates = [
+    {
+        "ccy":"EUR",
+        "base_ccy":"UAH",
+        "buy":"47.99000",
+        "sale":"48.99000"
+        },
+
+    {
+        "ccy":"USD",
+        "base_ccy":"UAH",
+        "buy":"40.95000",
+        "sale":"41.55000"}
+     ]
+
+def convert_currency():
+    inputNumber = float(input("Введіть вашу стипендію (в гривнях): "))
+
+    for rate in exchange_rates:
+        currency = rate["ccy"]
+        buy_rate = float(rate["buy"])
+        
+        result = inputNumber / buy_rate
+        print(f"Ваша стипендія у {currency}: {result:.2f}")
+
+convert_currency()
+
+            
+            
+
+            
+
+
+    
